9a60dbf3f546ba3cc8b4ad3443b1452b0a07230a
Muneeb <muhammadmuneebulraza@gmail.com>
2024-07-15 00:01:15 -0700

    update side Bar extend on menu btn

--- src/Components/Side_bar/SideBar.jsx
+++ src/Components/Side_bar/SideBar.jsx
@@ -1,38 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const SideBar = () => {
+  const [extendsidebar,setextendsidebar] = useState(false);
   return (
     <div className="bg-blue-500 px-3 h-screen flex flex-col justify-between w-full md:w-max text-white">
       {/* upper menu */}
       <div className="inline-flex flex-col space-y-5 mt-5">
         <div className="text-4xl mb-10 cursor-pointer">
-          <assets.IoMdMenu />
+          <assets.IoMdMenu onClick={()=>{setextendsidebar(prev=>!prev)}} />
         </div>
         <div className="flex items-center bg-blue-700 rounded-3xl p-3 justify-evenly cursor-pointer hover:bg-blue-600 transition">
-          <assets.FaPlus /> <p>New Chat</p>
+          <assets.FaPlus /> {extendsidebar? <p>New Chat</p>:null}
         </div>
+         {extendsidebar?
         <div className="mt-10">
-          <p className="text-lg font-semibold">Recent</p>
-          <div className="flex items-center gap-3 mt-3">
-            <assets.FiMessageSquare />
-            <p>There is a recent chat</p>
-          </div>
+        <p className="text-lg font-semibold">Recent</p>
+        <div className="flex items-center gap-3 mt-3">
+          <assets.FiMessageSquare />
+          <p>There is a recent chat</p>
         </div>
+      </div>:null 
+        }
+        
       </div>
       {/* lower menu */}
       <div className="mb-5 space-y-5">
         <div className="flex items-center text-md gap-3 cursor-pointer hover:text-blue-300 transition">
           <assets.FaQuestion />
-          <p>Help</p>
+        
+        {extendsidebar?<p>Help</p>:null}  
         </div>
         <div className="flex items-center text-md gap-3 cursor-pointer hover:text-blue-300 transition">
           <assets.IoCompassOutline />
-          <p>Activity</p>
+        
+        {extendsidebar?<p>Activity</p>:null}  
         </div>
         <div className="flex items-center text-md gap-3 cursor-pointer hover:text-blue-300 transition">
           <assets.IoSettingsOutline />
-          <p>Settings</p>
+        
+        {extendsidebar?<p>Settings</p>:null}  
         </div>
       </div>
     </div>
